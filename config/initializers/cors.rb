955a18a4e814297b08f0ad0491df6578286e21ef
Evan Powell <powellevan88@gmail.com>
2018-12-04 16:04:08 -0800

    cors authentication added.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
+
+
     resource '*',
       headers: :any,
+      expose: :authorization,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
