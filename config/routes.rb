a6c1f16dbe9b42733699aa567a6e6ab34a5de532
learn <krisnmccloud@gmail.com>
2018-12-12 13:48:54 -0800

    fixed registration form, creating user also creates profile & added backend error messages to UI for registration form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'user/my_tasks/:user_id' => 'my_tasks#show_user_tasks'
 
   devise_for :users, defaults: { format: :json }
+
   resources :tasks
   resources :my_tasks
   resources :profiles
