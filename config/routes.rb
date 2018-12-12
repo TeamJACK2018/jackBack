8a07cf405205cc949f7a4077d9253ced94286f96
learn <krisnmccloud@gmail.com>
2018-12-12 15:58:04 -0800

    fixed front end validation on register form. also started on API endpoint my_tasks

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   devise_for :users, defaults: { format: :json }
 
   resources :tasks
-  resources :my_tasks
+  resources :my_tasks 
   resources :profiles
   resources :appointments
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
