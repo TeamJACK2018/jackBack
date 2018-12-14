38d91c28ec703114e4152d0a282c7e3dd4faadd8
learn <ChristopherJStubbs93@gmail.com>
2018-12-14 11:53:19 -0800

    Added message to appt text

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,6 +5,7 @@ class Appointment < ActiveRecord::Base
 
   belongs_to :my_task
 
+
   after_create :reminder
 
   # Notify our appointment attendee X minutes before the appointment time
@@ -13,7 +14,7 @@ class Appointment < ActiveRecord::Base
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     client = Twilio::REST::Client.new account_sid, ENV['TWILIO_AUTH_TOKEN']
     time_str = ((self.exact_time).localtime).strftime("%I:%M%p on %b. %d, %Y")
-    reminder = "Hi #{self.name}. Just a reminder that you have an appointment coming up at #{time_str}."
+    reminder = "Hi #{self.name}. TASK ALERT: This is your #{task_category} reminder: #{task_title}. You set your reminder for #{time_str}. To see details, check your profile for more information. \n -Own Up Grown Up"
     message = client.api.account.messages.create(
       :from => twilio_number,
       :to => self.phone_number,
@@ -27,4 +28,13 @@ class Appointment < ActiveRecord::Base
   end
 
   handle_asynchronously :reminder, :run_at => Proc.new { |i| i.when_to_run }
+
+  def task_title
+    self.my_task.task.title
+  end
+
+  def task_category
+    self.my_task.task.category
+  end
+
 end
