e457f9f0dffa9b0e2a42e43650527a2b5d2861a1
learn <ChristopherJStubbs93@gmail.com>
2018-12-18 10:39:51 -0800

    started backend model testing

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe Profile do
+  let(:profile) {Profile.create(first_name: 'evan', last_name: 'powell', phone:'111-111-1111')}
+
+  describe 'display name' do
+
+    it 'should display first and last name' do
+      expect(profile.display_name).to eq('evan powell')
+    end
+
+    it "should display first name without last name or blank padding" do
+      expect(profile.first_name).to eq('evan')
+    end
+
+    it "should display the phone number if no first or last name exists (also without blank padding)" do
+      expect(profile.phone).to eq('111-111-1111')
+    end
+  end
 end
