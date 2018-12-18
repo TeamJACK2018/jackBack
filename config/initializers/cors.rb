0fe90464440e43eb6355d8c8cdf96255c8be6012
learn <ChristopherJStubbs93@gmail.com>
2018-12-18 14:53:13 -0800

    getting deployed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      expose: :authorization,
+      expose: :authorization, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
