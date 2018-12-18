cc6b4c0134b79896ed3a2bf826058c7afbeef46a
JSP <julianne.peters@gmail.com>
2018-12-18 13:56:58 -0800

    added misc, money, and medical to table

--- db/migrate/20181218215228_add_misc_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddMiscToProfile < ActiveRecord::Migration[5.2]
-  def change
-    add_column :profiles, :misc, :string
-  end
-end
--- db/migrate/20181218215252_add_financial_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddFinancialToProfile < ActiveRecord::Migration[5.2]
-  def change
-    add_column :profiles, :financial, :string
-  end
-end
